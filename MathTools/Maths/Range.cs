2c026ca262f69078c8b9b3acc5220a28f2a7b853
Drkrtl <drkrthanlife@gmail.com>
2020-02-03 21:08:39 +0800

    added range class

---
+++ MathTools/Maths/Range.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTools
+{
+    namespace Maths
+    {
+        [System.Serializable]
+        public class Range<T>
+        {
+            [SerializeField]
+            public T Min;
+            [SerializeField]
+            public T Max;
+
+            protected Range(T min, T max)
+            {
+                this.Min = min;
+                this.Max = max;
+            }
+
+            public static Range<int> IntRange(int min, int max)
+            {
+                return new Range<int>(min, max);
+            }
+
+            public static Range<float> FloatRange(float min, float max)
+            {
+                return new Range<float>(min, max);
+            }
+        }
+    }
+}
