8c6c2306b95538161e82a4b875cda5a27969d2b8
Drkr <drkrthanlife@gmail.com>
2020-03-31 02:05:01 +0800

    Create audio manager

---
+++ Management/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // keep pool of audio objects
+    // allows a 
+    AudioSource[] audioPool;
+    const int maxPool = 15;
+
+    int currentIdx;
+
+    void Awake()
+    {
+        audioPool = new AudioSource[maxPool];
+        for (int i = 0; i < audioPool.Length; i++)
+        {
+            audioPool[i] = gameObject.AddComponent<AudioSource>();
+            audioPool[i].playOnAwake = false;
+            audioPool[i].loop = false;
+            audioPool[i].spatialBlend = 0.0f;
+        }
+
+        currentIdx = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public AudioSource CreateAudio(AudioClip clip)
+    {
+        currentIdx = (currentIdx + 1) % maxPool;
+        
+        audioPool[currentIdx].clip = clip;
+        audioPool[currentIdx].Play();
+
+        return audioPool[currentIdx];
+    }
+}
